81ee5b263338486a062e14184dc71c5568a2e2e9
will <will@buildwithme.biz>
2026-03-24 19:16:16 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import SuperAdminHome from './pages/SuperAdminHome';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
