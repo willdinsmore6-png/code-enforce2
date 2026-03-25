903da380baa3b2cbdfd9e7241f8ba285a906a255
will <will@buildwithme.biz>
2026-03-25 12:48:30 +0000

    File changes

--- base44/functions/getUsers/entry.ts
+++ base44/functions/getUsers/entry.ts
@@ -6,18 +6,7 @@ Deno.serve(async (req) => {
     const user = await base44.auth.me();
     if (!user) return Response.json({ error: 'Unauthorized' }, { status: 401 });
 
-    const allUsers = await base44.asServiceRole.entities.User.list();
-
-    let users;
-    if (user.role === 'superadmin') {
-      // Superadmin sees all users
-      users = allUsers;
-    } else if (user.municipality_id) {
-      // Admins and staff only see their municipality's users
-      users = allUsers.filter(u => u.municipality_id === user.municipality_id);
-    } else {
-      users = [];
-    }
+    let users = await base44.asServiceRole.entities.User.list();
 
     // Exclude the hardcoded build account
     users = users.filter(u => u.email !== 'will@buildwithme.biz');
