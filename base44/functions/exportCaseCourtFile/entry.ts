376a5b34101e10cb4f7ac3ae810a0734c3371452
will <will@buildwithme.biz>
2026-03-26 20:28:51 +0000

    File changes

--- base44/functions/exportCaseCourtFile/entry.ts
+++ base44/functions/exportCaseCourtFile/entry.ts
@@ -1,5 +1,5 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.23';
-import jsPDF from 'npm:jspdf@4.0.0';
+import { jsPDF } from 'npm:jspdf@4.0.0';
 
 Deno.serve(async (req) => {
   try {
