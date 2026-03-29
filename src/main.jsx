59c01a5caaf9334a705ece4724ee999a9e0933a8
willdinsmore6-png <willdinsmore6@gmail.com>
2026-03-29 10:41:16 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './lib/AuthContext'
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
