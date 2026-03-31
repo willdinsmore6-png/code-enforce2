33a64f594d8cd1cca171b8be81856720484038d0
willdinsmore6-png <willdinsmore6@gmail.com>
2026-03-31 07:01:32 -0400

    Fix Case Volume Trend tooltip z-index and overflow issues

---
+++ SuperAdminDashboard.jsx
@@ -0,0 +1,8 @@
+<div className="flex items-end justify-between h-full gap-1.5 px-2 overflow-visible relative">
+    {/* Other components inside the container */}
+</div>
+
+{/* Tooltip component with updated className */}
+<div className="tooltip z-50 pointer-events-none">
+    {/* Tooltip content */}
+</div>
